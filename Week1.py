e78527611ace3abaeb5bcb180d9e3424ef360e5c
Marcus Herstik <marcus@herstik.com>
2023-01-25 16:46:40 +1000

    Create Week1.py

---
+++ Week1.py
@@ -0,0 +1,23 @@
+a = 4
+b = "Marcus's t-shirt"
+c = '12'
+d = 4.0
+
+print(a)
+print(c * a)
+
+print(type(a))
+# this is a comment
+
+c = int(c)
+
+print("C is",c,"and is a type", type(c))
+# change to int
+print("C is",str(c),"and is a type", type(str(c)))
+print("C is", float(c),"and is a type", type(float(c)))
+
+
+
+print(type(d))
+print(d * a)
+
