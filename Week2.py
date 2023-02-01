6a769216a81d4de0f2068e67c599a8b0e64ae00b
Marcus Herstik <marcus@herstik.com>
2023-02-01 17:00:09 +1000

    Create Week2.py

---
+++ Week2.py
@@ -0,0 +1,40 @@
+# firstName = "Marcus"
+# surname = "Herstik"
+# b = 4
+# c = 4.0
+# print(b)
+# print(type(firstName),type(b),type(c))
+# 
+# print(firstName + surname)
+
+
+name = input("Please provide your name: ")
+
+print("Hello", name, "pleased to meet you!")
+
+## ask for the users age and store as variable age
+
+age = input("Please provide your age: ")
+
+# tell the person how old they will be next year
+
+#print(name, "you will be", age + 1, "next year")
+
+
+# if name == "Tom":
+#     print("Way to go Mr Jones")
+# elif name != "Marcus":
+#     print("YOu aren't Marcus")
+#     surname = input("Give me your surname: ")
+# else:
+#     print("You aren't Tom Jones. Go away!!")
+
+
+# if type(age) is int:
+#     print("It's an integer")
+# if type(age) is not int:
+#     print("It's not an integer")
+    
+    
+    
+    
