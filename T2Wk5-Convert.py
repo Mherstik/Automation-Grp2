969aaccc7355c1f3f49ea378ce2b47bdf87610cc
Marcus Herstik <marcus@herstik.com>
2023-05-17 17:40:26 +1000

    Update T2Wk5-Convert.py

--- T2Wk5-Convert.py
+++ T2Wk5-Convert.py
@@ -1,7 +1,7 @@
 import string
-a = input("Text to convert: ")
+userText = input("Text to convert: ")
 shift = int(input("Numshifter to shift: "))
-for each in a:
+for each in userText:
         x = string.ascii_uppercase.index(each)
         print(each,"is position", x, "+", shift, " = ", x+shift)
         print("More than A-Z shiftecomes", (x+shift) % 26, "=",string.ascii_uppercase[((x+shift)%26)] )
